b26e5855d9401df9401480e4d2997400d69128e3
kharina21 <127693983+kharina21@users.noreply.github.com>
2025-02-09 22:45:56 +0700

    setup routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { publicRoutes } from "~/routes";
 function App() {
     return (
-        <div>
-            <h1>React App</h1>
-        </div>
+        <Router>
+            <div className="App">
+                <ul>
+                    <li>
+                        <Link to="/">Home page</Link>
+                    </li>
+                    <li>
+                        <Link to="/following">Following page</Link>
+                    </li>
+                    <li>
+                        <Link to="/profile">Profile page</Link>
+                    </li>
+                </ul>
+                <Routes>
+                    {publicRoutes.map((route, index) => {
+                        const Page = route.component;
+                        return (
+                            <Route
+                                key={index}
+                                path={route.path}
+                                element={<Page />}
+                            />
+                        );
+                    })}
+                </Routes>
+            </div>
+        </Router>
     );
 }
 
