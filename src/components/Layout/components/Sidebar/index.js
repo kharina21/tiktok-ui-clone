2b8923c85cb614cd63ce61efd28ed9f491815dff
kharina21 <127693983+kharina21@users.noreply.github.com>
2025-02-10 03:39:02 +0700

    design UI

--- src/components/Layout/components/Sidebar/index.js
+++ src/components/Layout/components/Sidebar/index.js
@@ -1,5 +1,12 @@
+import styles from "./Sidebar.module.scss";
+import classNames from "classnames/bind";
 function Sidebar() {
-    return <h2>Sidebar</h2>;
+    const cx = classNames.bind(styles);
+    return (
+        <aside className={cx("wrapper")}>
+            <h2>Sidebar</h2>
+        </aside>
+    );
 }
 
 export default Sidebar;
